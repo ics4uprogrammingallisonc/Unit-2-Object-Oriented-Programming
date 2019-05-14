1408c57b3b2c5d766d43e5ae31fdc427eaaefcca
Allison <allison.cook@ocsbstudent.ca>
2019-05-14 12:36:12 -0400

    Day 22 Inheritence

--- Day 22 Inheritance/src/Main.java
+++ Day 22 Inheritance/src/Main.java
@@ -14,9 +14,15 @@ public class Main {
 		int numDoors = 0;
 		String colour;
 		String plateNumber;
+		int wheels = 0;
+		int lenght = 0;
+		int seats = 0;
 		
 		//scanner
 		Scanner read = new Scanner(System.in);
+		
+		//start with the truck
+		System.out.println("Your truck:");
 		
 		//get info from user
 		//get the variables from the user
@@ -47,16 +53,100 @@ public class Main {
 			}
 		}
 		
-		//vehicle class
-		//Vehicle myObject = new Vehicle(plateNumber, colour, numDoors);
+		//until they enter a int
+			while(wheels == 0) {
+				System.out.println("Number of wheels:");
+					
+				//wait before asking again
+				TimeUnit.SECONDS.sleep(5);
+					
+				try {
+					//get the input
+					wheels = Integer.parseInt(read.nextLine());
+				}
+				catch(Exception e)
+				{
+					//display error message if not an int
+					System.out.println("Please enter an integer.");
+				}
+			}
+			
+			//until they enter a int
+			while(lenght == 0) {
+				System.out.println("Length:");
+					
+				//wait before asking again
+				TimeUnit.SECONDS.sleep(5);
+					
+				try {
+					//get the input
+					lenght = Integer.parseInt(read.nextLine());
+				}
+				catch(Exception e)
+				{
+					//display error message if not an int
+					System.out.println("Please enter an integer.");
+				}
+			}
 		
-		//myObject.Display();
+		//get truck class
+		Truck myTruck = new Truck(plateNumber, colour, numDoors, wheels, lenght);
 		
-		Truck myTruck = new Truck(plateNumber, colour, numDoors);
+		//start the bike
+		System.out.println("Your Bike:");
 		
-		myTruck.Display();
+		//get the colour
+		System.out.println("Colour:");
+		//get the input 
+		colour = read.nextLine();
 		
-		System.out.println();
+		//get the number of wheels
+		while(wheels == 0) {
+			System.out.println("Number of wheels:");
+				
+			//wait before asking again
+			TimeUnit.SECONDS.sleep(5);
+				
+			try {
+				//get the input
+				wheels = Integer.parseInt(read.nextLine());
+			}
+			catch(Exception e)
+			{
+				//display error message if not an int
+				System.out.println("Please enter an integer.");
+			}
+		}
+		
+		
+		//get the number of seats
+		while(seats == 0) {
+			System.out.println("Number of seats:");
+				
+			//wait before asking again
+			TimeUnit.SECONDS.sleep(5);
+				
+			try {
+				//get the input
+				seats = Integer.parseInt(read.nextLine());
+			}
+			catch(Exception e)
+			{
+				//display error message if not an int
+				System.out.println("Please enter an integer.");
+			}
+		}
+		
+		//get the bike class
+		Bike myBike = new Bike(colour, seats, wheels);
+		
+		
+			//display the new truck
+			myTruck.Display();
+				
+			//display the bike
+			myBike.Display();
+				
 		
 	}
 
@@ -70,11 +160,11 @@ class Vehicle{
 		colour = tempColour;
 		numDoors = tempnumDoors;
 	}	
-	private String plateNumber = "";
-	private String colour = "";
-	private int numDoors = 0;
-	private int speed = 0;
-	private final int MAX_SPEED = 80;
+	protected String plateNumber = "";
+	protected String colour = "";
+	protected int numDoors = 0;
+	protected int speed = 0;
+	protected final int MAX_SPEED = 80;
 	
 	protected void Brake() {
 		//take away from the speed
@@ -92,6 +182,11 @@ class Vehicle{
 		}
 	}
 	
+	//get speed
+	public int getSpeed() {
+		return speed;
+	}
+	
 	public void Display() {
 		//output all the information on the car
 		System.out.println("Your Car"  + ": 1");
@@ -105,11 +200,55 @@ class Vehicle{
 }
 
 class Truck extends Vehicle{
+	
+	//add two more variables for the truck class
+	int numWheels = 0;
+	int lenght = 0;
 
-	public Truck(String plateNum, String tempColour, int tempnumDoors) {
+	public Truck(String plateNum, String tempColour, int tempnumDoors, int wheels, int tempLenght) {
 		super(plateNum, tempColour, tempnumDoors);
+		//set the other variables
+		numWheels = wheels;
+		lenght = tempLenght;
+	}	
 		
+	//display the truck
+	public void Display() {
+		//output all the information on the truck
+				System.out.println("Your truck");
+				System.out.println("Licence plate number: " + plateNumber);
+				System.out.println("Colour: " + colour);
+				System.out.println("Number of doors: " + numDoors);
+				System.out.println("Number of wheels: " + numWheels);
+				System.out.println("Length: " + lenght);
+				System.out.println("Speed: " + speed + "km/h");
+				System.out.println("Max Speed: " + MAX_SPEED + "km/h");
+	}
+}
+
+class Bike extends Vehicle{
+	
+	//variables
+	int seats;
+	int numWheels; 
+	final int MAX_SPEED = 40;
+
+	public Bike(String tempColour, int numSeats, int wheels) {
+		super("", tempColour, 0);
+		//set the other variables
+		numWheels = wheels;
+		seats = numSeats;
 	}
 	
+	//display the bike
+	public void Display() {
+		//output all the information on the bike
+		System.out.println("Your bike");
+		System.out.println("Colour: " + colour);
+		System.out.println("Number of wheels: " + numWheels);
+		System.out.println("Number of seats: " + seats);
+		System.out.println("Speed: " + speed + "km/h");
+		System.out.println("Max Speed: " + MAX_SPEED + "km/h");
+	}
 	
 }
